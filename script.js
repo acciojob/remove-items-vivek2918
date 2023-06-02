5e08ef26c9d254e5cd847e4acf61cb8406f3f862
Vivek Chovatiya <119297870+vivek2918@users.noreply.github.com>
2023-06-02 10:03:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+
+function removeItem() {
+	let selectedValue = document.getElementById('colorSelect').value;
+	let selectedItem = document.getElementById(selectedValue);
+	selectedItem.remove();
+}
